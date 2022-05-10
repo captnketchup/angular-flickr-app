6827ccabc659429531551dd91e073636be1691ae
captnketchup <74988054+captnketchup@users.noreply.github.com>
2022-05-10 17:15:52 +0200

    added key to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:  "ff6ff6c01949b173a7598e0bd1c20ff5" || ""
 };
--- src/services/constants.ts
+++
@@ -1,3 +0,0 @@
-export const apiKey = "ff6ff6c01949b173a7598e0bd1c20ff5" || ""
-console.log(apiKey)
-export const secretKey = "681855c3aea53e92"
